e287bd2a600518e82238e431b90cac721a0cf3fb
Dan Lentz <danlentz@github.com>
2013-08-07 09:42:29 -0400

    optimized: transactions/sec increased 700% to 900% in tests with 2 threads and maximal contention

--- dstm.lisp
+++ dstm.lisp
@@ -9,93 +9,128 @@
 ;;;;; Thanks also to pkhuong for assistance debugging :)
 ;;;;;
 
+;;(delete-package :dstm)
+
 (defpackage :dstm
-   (:use #:common-lisp)
-   (:export
-    #:var
-    #:create-var
-    #:read-var
-    #:write-var
-    #:write-vars
+  (:shadow :read :write :variable) 
+  (:use :common-lisp)
+  (:export
+    #:variable
+    #:make-value
+ ;   #:read-var
+ ;   #:write-var
+ ;   #:write-vars
     #:atomic
     #:orelse
+    #:check
     #:rollback
+    #:read
+    #:write  
     #:rmw
-    #:check))
+    
+    ))
 
 (in-package :dstm)
 
-(defparameter *transactions* (make-hash-table :test #'eq :synchronized t))
+;; (defparameter *transactions* (make-hash-table :test #'eq :synchronized t))
+;; (defvar *transaction* nil)
+; (defclass transaction ()
+;;   ((state   :accessor transaction-state  :initform :active  :initarg :state)
+;;     (root   :reader   transaction-root   :initform nil)
+;;     (reads  :accessor transaction-reads  :initform nil)
+;;     (subs   :accessor transaction-subs   :initform nil)
+;;     (parent :accessor transaction-parent :initform (current-transaction))))
+ ;; (defun current-transaction ()
+ ;;   (sb-thread:symbol-value-in-thread 'dstm::*transaction* sb-thread:*current-thread* nil))
+ ;; (defun (setf current-transaction) (trans)
+ ;;   (setf
+ ;;     #+sbcl (sb-thread:symbol-value-in-thread 'dstm::*transaction* sb-thread:*current-thread* nil)
+ ;;     trans))
+ ;; (defun current-transaction ()
+ ;;   (gethash sb-thread:*current-thread* *transactions*))
+ ;; (defun (setf current-transaction) (trans)
+ ;;   (setf (gethash sb-thread:*current-thread* *transactions*) trans))
+;; (defmethod initialize-instance :after ((this-transaction transaction) &key &allow-other-keys)
+;;   (setf (slot-value this-transaction 'root)
+;;     (let ((current-transaction (current-transaction)))
+;;       (if current-transaction
+;;         (transaction-root current-transaction)
+;;         this-transaction))))
 
-
-(defvar *transaction* nil)
-
-
-(defclass transaction ()
-  ((state   :accessor transaction-state  :initform :active  :initarg :state)
-    (root   :reader   transaction-root   :initform nil)
-    (reads  :accessor transaction-reads  :initform nil)
-    (subs   :accessor transaction-subs   :initform nil)
-    (parent :accessor transaction-parent :initform (current-transaction))))
-
-
-;; (defun current-transaction ()
-;;   (sb-thread:symbol-value-in-thread 'dstm:*transaction* sb-thread:*current-thread* nil))
-
-
-;; (defun (setf current-transaction) (trans)
-;;   (setf
-;;     #+sbcl (sb-thread:symbol-value-in-thread 'dstm:*transaction* sb-thread:*current-thread* nil)
-;;     trans))
-
-(defun current-transaction ()
-  (gethash sb-thread:*current-thread* *transactions*))
-
-(defun (setf current-transaction) (trans)
-  (setf (gethash sb-thread:*current-thread* *transactions*) trans))
-
-
-(defmethod initialize-instance :after ((this-transaction transaction) &key &allow-other-keys)
-  (setf (slot-value this-transaction 'root)
-    (let ((current-transaction (current-transaction)))
-      (if current-transaction
-        (transaction-root current-transaction)
-        this-transaction))))
-
-(defun equivalentp (trans1 trans2)
-   (eq (transaction-root trans1) (transaction-root trans2)))
-
-
-(defclass var ()
-   ())
-
-
-(defclass dstm-var (var)
-  ((new
-     :accessor dstm-var-new
-     :initform nil
-     :initarg :new)
-    (old
-      :accessor dstm-var-old
-      :initform nil)
-    (trans
-      :accessor dstm-var-trans
-      :initform (load-time-value (make-instance 'transaction :state :committed)))))
-
-
-(defun create-var (&optional val)
-   (make-instance 'dstm-var :new val))
-
-
-(define-condition rollback-exn ()
-   ())
-
-(defun reclaim-lists (trans)
-  (setf (transaction-reads  trans) nil
-    (transaction-subs   trans) nil))
 
 (defparameter *nrolls* (make-array '(1) :element-type 'sb-ext:word :initial-element #x0))
 (defparameter *ntrans* (make-array '(1) :element-type 'sb-ext:word :initial-element #x0))
+
+(tls:define .transaction. nil)
+
+(defun current-transaction ()
+  (tls:symbol-value '.transaction.))
+
+(defun (setf current-transaction) (trans)
+  (setf (tls:symbol-value '.transaction.) trans))
+  
+(defstruct (transaction (:constructor %make-transaction))
+  (state  :active)
+  (parent (current-transaction))
+  (root   nil)
+  (reads  nil)
+  (subs   nil))
+
+(defmethod print-object ((self transaction) stream)
+  (print-unreadable-object (self stream :type nil :identity t)
+    (format stream "~A ~S (~D reads, ~D subs)"
+      (if (eq self (transaction-root self))
+        :root-transaction
+        :transaction)
+      (transaction-state self)
+      (length (transaction-reads self))
+      (length (transaction-subs self))
+      )))
+
+(defun make-transaction (&key (state :active))
+  (let ((this-transaction (%make-transaction :state state)))
+    (setf (transaction-root this-transaction)
+      (let ((current-transaction (current-transaction)))
+        (if current-transaction
+          (transaction-root current-transaction)
+          this-transaction)))
+    this-transaction))
+                          
+(defun equivalentp (trans1 trans2)
+   (eq (transaction-root trans1) (transaction-root trans2)))
+
+(defclass variable ()
+   ())
+
+(defclass transactional-variable (variable)
+  ((new
+     :accessor new-value-of
+     :initform nil
+     :initarg :new)
+    (old
+      :accessor old-value-of
+      :initform nil)
+    (trans
+      :accessor transaction-of
+      :initform (load-time-value (make-transaction :state :committed)))))
+
+(defun make-value (&optional value)
+  (make-instance 'transactional-variable :new value))
+
+(defmethod print-object ((self transactional-variable) stream)
+  (print-unreadable-object (self stream :type  nil :identity nil)
+    (if (eq :committed (transaction-state (transaction-of self)))
+      (format stream "TRANSACTIONAL-VALUE: ~S" (new-value-of self))
+      (format stream "TRANSACTIONAL-VALUE: ~S" (old-value-of self)))
+    (format stream " [~A]" (transaction-state (transaction-of self)))))
+      
+(define-condition rollback-execution ()
+   ())
+
+(defun reclaim-lists (trans)
+  (setf
+    (transaction-reads trans) nil
+    (transaction-subs  trans) nil))
 
 
 (defun rollback-trans-and-subs (trans)
@@ -110,21 +145,17 @@
     (when trans      
       (sb-ext:atomic-incf (aref *nrolls* 0))
       (rollback-trans-and-subs trans)
-      (error (load-time-value (make-condition 'rollback-exn) t)))))
+      (error (load-time-value
+               (make-condition 'rollback-execution) t)))))
 
 
 (defun check-reads (trans)
   (dolist (pair (transaction-reads trans))
     (destructuring-bind (var . vtrans) pair
-      (let ((vnow (dstm-var-trans var)))
+      (let ((vnow (transaction-of var)))
         (unless (or (eq vnow vtrans)          ;; unchanged?
                   (equivalentp trans vnow))   ;; ...or changed by us...
           (rollback))))))
-
-
-(defun commit (final)
-   (commit-with-transaction (current-transaction) final))
-
 
 (defun commit-with-transaction (trans final)
    (dolist (sub (transaction-subs trans))
@@ -139,47 +170,52 @@
      (setf (transaction-state trans) :COMMITTED)
      (reclaim-lists trans)))
 
+(defun commit (final)
+  (commit-with-transaction (current-transaction) final))
 
-(defun read-var (var)
+(defun read (var)  
   (let ((trans (current-transaction)))
     (loop
-      (let* ((vtrans (dstm-var-trans var))
+      (let* ((vtrans (transaction-of var))
               (vstate (transaction-state vtrans)))
         (when (or
                 (not (eq :ACTIVE vstate))
                 (and trans (equivalentp trans vtrans)))
           (when trans (push (cons var vtrans) (transaction-reads trans)))
           (return (if (eq :ABORTED vstate)
-                    (dstm-var-old var)
-                    (dstm-var-new var))))))))
+                    (old-value-of var)
+                    (new-value-of var))))))))
 
-
-(defun write-var (var val)
-   (let* ((trans   (current-transaction))
-          (wtrans  (or trans (make-instance 'transaction))))
-     (prog1 (write-var-with-transaction wtrans var val)
-       (unless trans (setf (transaction-state wtrans) :COMMITTED)))))
-
-
-(defun write-vars (&rest pairs)
-   (do ((pairs pairs (cddr pairs)))
-       ((null pairs))
-     (write-var (car pairs) (cdar pairs))))
-
+(defun read-var (var)
+  (read var))
 
 (defun write-var-with-transaction (trans var val)
   "trans is the current transaction for a thread"
   (loop
-    (let* ((vtrans (dstm-var-trans var))
+    (let* ((vtrans (transaction-of var))
             (vstate (transaction-state vtrans)))
       (cond
-        ((not (eq :ACTIVE vstate))  (when (eq (sb-ext:cas (slot-value var 'trans) vtrans trans)
-                                            vtrans)
-                                      (when (eq :COMMITTED vstate)
-                                        (setf (dstm-var-old var) (dstm-var-new var)))
-                                      (return (setf (dstm-var-new var) val))))
-        ((equivalentp trans vtrans) (return (setf (dstm-var-new var) val)))))))
+        ((not (eq :ACTIVE vstate))
+          (when (eq (sb-ext:compare-and-swap (slot-value var 'trans) vtrans trans) vtrans)
+            (when (eq :COMMITTED vstate)
+              (setf (old-value-of var) (new-value-of var)))
+            (return (setf (new-value-of var) val))))
+        ((equivalentp trans vtrans)
+          (return (setf (new-value-of var) val)))))))
 
+(defun write (var val)
+  (let* ((trans   (current-transaction))
+          (wtrans  (or trans (make-transaction))))
+    (prog1 (write-var-with-transaction wtrans var val)
+      (unless trans (setf (transaction-state wtrans) :COMMITTED)))))
+
+(defun write-var (var val)
+  (write var val))
+
+(defun write-vars (&rest pairs)
+  (do ((pairs pairs (cddr pairs)))
+    ((null pairs))
+    (write (car pairs) (cdar pairs))))
 
 (defun do-orelse (&rest fns)
   "Perform one of the functions in the list fns.  The list is
@@ -189,13 +225,12 @@
   (let ((ct-save (current-transaction)))
     (loop
       (dolist (fn fns)
-        (setf (current-transaction)
-          (make-instance 'transaction))
+        (setf (current-transaction) (make-transaction))
         (sb-ext:atomic-incf (aref *ntrans* 0))
         (unwind-protect (handler-case (return-from do-orelse
                                         (prog1 (funcall fn)
                                           (commit (null ct-save))))
-                          (rollback-exn (exn)
+                          (rollback-execution (exn)
                             (declare (ignore exn)))
                           (error (exn)
                             (rollback-trans-and-subs (current-transaction))
@@ -222,7 +257,7 @@
 
 (defun do-rmw (place fn)
   "RMW = read / modify / write"
-  (atomic (write-var place (funcall fn (read-var place)))))
+  (atomic (write place (funcall fn (read place)))))
 
 
 (defmacro rmw ((var-name place) &body body)
@@ -248,8 +283,8 @@
   (setf (aref  *nrolls* 0) 0)
   (setf (aref  *ntrans* 0) 0))
 
-(defvar *a* (create-var 0))
-(defvar *b* (create-var 0))
+(defvar *a* (make-value 0))
+(defvar *b* (make-value 0))
 
 (defvar *up-remaining*   0)
 (defvar *down-remaining* 0)
@@ -257,16 +292,16 @@
 (defun check-invariant (&aux x y)
   (atomic
     (setf
-      x (read-var *a*)
-      y (read-var *b*)))
+      x (read *a*)
+      y (read *b*)))
   (unless (= y (* 2 x))    
     (error "~&Invariant broken: A = ~A, B = ~A~%" x y)))
 
 (defun common-code (delta)
   (atomic
-    (let ((a (+ delta (read-var *a*))))
-      (write-var *a* a)
-      (write-var *b* (* 2 a)))))
+    (let ((a (+ delta (read *a*))))
+      (write *a* a)
+      (write *b* (* 2 a)))))
 
 (defun test-dstm (&optional (iterations 1000000))
   (flet ((count-up ()
@@ -281,8 +316,8 @@
             (loop until (and (eql *up-remaining* 0) (eql *down-remaining* 0))
               do (check-invariant))))
     (format *trace-output* "~&Start DSTM Test...~%")
-    (setf *a* (create-var 0))
-    (setf *b* (create-var 0))
+    (setf *a* (make-value 0))
+    (setf *b* (make-value 0))
     (setf *up-remaining* iterations)
     (setf *down-remaining* iterations)
     (reset)
@@ -296,6 +331,31 @@
         (princ (show-rolls  (/ (- stop start) internal-time-units-per-second))
           *trace-output*)))))
 
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; current results
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; (test-dstm 50000)
+;; (:ROLLBACKS 41543 :TRANSACTIONS 166679 :PERCENT-ROLLBACKS 24.923956
+;;   :TRANS-PER-ROLL 4.012204 :DURATION 0.186 :TRANS-PER-SEC 896123.6)
+
+;; (test-dstm 100000)
+;; (:ROLLBACKS 100717 :TRANSACTIONS 341761 :PERCENT-ROLLBACKS 29.470009
+;;   :TRANS-PER-ROLL 3.39328 :DURATION 0.31 :TRANS-PER-SEC 1102454.9)
+
+;; (test-dstm 500000)
+;; (:ROLLBACKS 397331 :TRANSACTIONS 1650343 :PERCENT-ROLLBACKS 24.075663
+;;   :TRANS-PER-ROLL 4.153572 :DURATION 1.658 :TRANS-PER-SEC 995381.8)
+
+;; (test-dstm 1000000)
+;; (:ROLLBACKS 826858 :TRANSACTIONS 3295458 :PERCENT-ROLLBACKS 25.090837
+;;   :TRANS-PER-ROLL 3.9855187 :DURATION 3.263 :TRANS-PER-SEC 1009947.3)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; prior results
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; (test-dstm 50000)
 ;; (:ROLLBACKS 30657 :TRANSACTIONS 153278 :PERCENT-ROLLBACKS 20.000914
@@ -312,3 +372,5 @@
 ;; (test-dstm 1000000)
 ;; (:ROLLBACKS 624829 :TRANSACTIONS 3069373 :PERCENT-ROLLBACKS 20.356894
 ;;   :TRANS-PER-ROLL 4.912341 :DURATION 21.683 :TRANS-PER-SEC 141556.66)
+
+
